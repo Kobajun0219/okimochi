593b42566e5927c967a2c595125f40f035601604
EC2 Default User <ec2-user@ip-172-31-37-134.us-east-2.compute.internal>
2021-06-30 15:40:52 +0000

    sandome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'oki-Mochi') }}</title>
+    <title>oki-Mochi</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
